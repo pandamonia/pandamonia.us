05ba33b5b87f87b702d89318480d4d036319ba11
Alexsander Akers <a2@pandamonia.us>
2013-02-27 20:46:41 -0500

    Try to get these exceptions fixed
    Signed-off-by: Alexsander Akers <a2@pandamonia.us>

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require 'zurb-foundation'
 
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "http://pandamonia.us/"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
