953d299462aaf770fc8ea7c7173b0a9c475d723f
Alexsander Akers <a2@pnd.mn>
2014-01-27 17:11:15 -0800

    Site generated at 2014-01-28 01:11:15 UTC

---
+++ signup.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="refresh" content="0;http://eepurl.com/cifzH" />
+	<script>
+		window.location.href = "http://eepurl.com/cifzH";
+	</script>
+</head>
+<body>
+	<a href="http://eepurl.com/cifzH">Sign up here.</a>
+</body>
+</html>
